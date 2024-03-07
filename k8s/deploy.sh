1af85ac62a99d531cb4b540b1b3a4f6c0ba0fdeb
kimkun07 <kimkun07@naver.com>
2024-03-07 06:03:56 +0000

    Backend ready?

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,6 +1,9 @@
 kubectl delete ingresses.v1.networking.k8s.io nginx-ing
 kubectl delete svc nginx-svc
 kubectl delete svc frontend-svc
+kubectl delete svc backend-svc
 kubectl delete deploy nginx-dep
 kubectl delete deploy frontend-dep
+kubectl delete deploy backend-dep
+kubectl get cm --no-headers=true | awk '/nginx/{print $1}'| xargs  kubectl delete cm
 kubectl apply -k .
